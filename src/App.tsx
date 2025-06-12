33b579465f743c69e772158365e181e796ab1825
koo-rogie <rn5184@naver.com>
2025-06-13 01:23:31 +0900

    🎉Init : 프로젝트 초기 생성

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import Attendance from "./Attendance/Attendance.tsx";
+// import Bank from "./compononts/Bank.tsx";
+
+function App() {
+  return (
+    <>
+      {/* <Bank /> */}
+      <Attendance />
+    </>
+  );
+}
+
+export default App;
